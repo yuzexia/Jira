04ef6d886ffb63578c79c1df43ea85c3ddcfb4a8
yuze.xia <327932801@qq.com>
2021-09-03 14:14:29 +0800

    search-panel

--- src/screens/project-list/search-panel.jsx
+++ src/screens/project-list/search-panel.jsx
@@ -1,5 +1,35 @@
+import { useEffect, useState } from "react"
+
 export const SearchPanel = () => {
+    const [param, setParam] = useState({
+        name: '',
+        personId: ''
+    })
+    const [users, setUsers] = useState([]);
+    const [list, setList]   = useState([])
+    useEffect(() => {
+        fetch('').then(async response => {
+            if(response.ok) {
+                setList(await response.json());
+            }
+        })
+    }, [param])
     return (
-        <form action=""></form>
+        <form>
+            {/* setParam(Object.assign({}, param, {name: evt.target.value})) */}
+            <input type='text' value={param.name} onChange={evt => setParam({
+                ...param,
+                value: evt.target.value
+            })} />
+            <select value={param.personId} onChange={evt => setParam({
+                ...param,
+                value: evt.target.value
+            })}>
+                <option value={''}>负责人</option>
+                {
+                    users.map(user => <option value={user.id}>{user.name}</option>)
+                }
+            </select>
+        </form>
     )
 }
